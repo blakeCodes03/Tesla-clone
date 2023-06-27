18e463f2b428fa491ee7a0c50518835b7920ab91
blakeCodes03 <jahotanenlap@gmail.com>
2023-06-27 18:46:12 +0100

    fixed navbar display & CTA button size on sm screens

--- src/components/Models.jsx
+++ src/components/Models.jsx
@@ -1,22 +1,7 @@
-import React, { useRef, createContext, useCallback, useContext } from "react";
-// import Navbar from "./Navbar";
-import useIntersection from "./useIntersection";
+import React from "react";
 import { FiChevronDown } from "react-icons/fi";
-// import { TfiClose } from "react-icons/tfi";
-
-// const MyContext = React.createContext();
-
-// Navbar section begins
 
 function Models(props) {
-  // const[showMenu, setShowMenu] = React.useState(false);
-
-  // function toggleMenu() {
-  //   setShowMenu(prevState=>!prevState);
-  //   console.log(showMenu);
-
-  // }
-
   let backgroundUrl;
   switch (props.id) {
     case 1:
@@ -58,74 +43,9 @@ function Models(props) {
   }
   const addBlur = props.showMenu ? " blur-sm" : " ";
 
-  /* this block implements useIntersection with useRef()
-  to add and remove <Navbar/> as <Models/> elements enter and leave viewport*/
-  // const ref = useRef();
-
-  // const inViewport = useIntersection(ref, "200px"); // Trigger as soon as the element becomes visible
-
   return (
-    <div
-      
-      className={backgroundUrl + addBlur + " relative" + "  models"}
-    >
-      
-        
-          {/* {
-          props.showMenu && <div className={`bg-white absolute right-0 top-0 w-80 no-blur z-50  overflow-y-auto menu-div ${props.toggleMenu ? " " : " "}`}>
-          <div className="flex justify-end pr-8 pt-8">
-            <TfiClose 
-            onClick={()=>props.toggleMenu(props.id)}           
-              className="rounded p-1 hover:bg-black/5"
-              size={28}
-            />
-          </div>
-          <ul className="pt-8 px-6">
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://tesla.com/models">Model S</a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://tesla.com/model3">Model 3</a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://tesla.com/modelx">Model X</a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://tesla.com/modely">Model Y</a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://tesla.com/solarroof">Solar Roof</a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://tesla.com/solarpanels">Solar Panels</a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://www.tesla.com/inventory/new/m3">
-                Existing Inventory
-              </a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://www.tesla.com/inventory/used/m3">Used Inventory</a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://www.tesla.com/tradein">Trade-In</a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://www.tesla.com/drive">Test Drive</a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://www.tesla.com/insurance">Insurance</a>
-            </li>
-            <li className="py-3 pl-3 hover:rounded hover:bg-black/5">
-              <a href="https://www.tesla.com/powerwall">Powerwall</a>
-            </li>
-          </ul>
-        </div>
-        } */}
-        {/* </div> */}
-    
-
-      <div className="absolute inset-x-0 mt-[50px] lg:mt-[80px] text-center ">
+    <div className={backgroundUrl + addBlur + " relative" + "  models"}>
+      <div className="absolute inset-x-0 mt-[100px] lg:mt-[80px] text-center ">
         <h1 className="text-4xl font-bold">{props.item}</h1>
         <p className="text-sm underline underline-offset-4 hover:cursor-pointer hover:underline-offset-5 hover:decoration-2 hover:decoration-gray-800">
           <a href="">{props.cta}</a>
@@ -133,14 +53,14 @@ function Models(props) {
       </div>
       <div className="absolute flex flex-col items-center justify-center text-sm lg:flex-row inset-x-0 bottom-[10%]">
         <a href="https://www.tesla.com/model3/design">
-          <button className="rounded-md bg-[#393c41] w-96 lg:w-64 h-10 lg:mx-4 mt-2 text-white opacity-100 font-semibold">
+          <button className="rounded-md bg-[#393c41] w-80 lg:w-64 h-10 lg:mx-4 mt-2 text-white opacity-100 font-semibold">
             {props.darkButton}
           </button>
         </a>
         {/* to display the lightButton only if there is a value for it */}
         {props.lightButton && (
           <a href="https://www.tesla.com/model3/design">
-            <button className="rounded-md bg-[#f4f4f4] w-96 lg:w-64 h-10 lg:mx-4 mt-2 opacity-60 font-semibold">
+            <button className="rounded-md bg-[#f4f4f4] w-80 lg:w-64 h-10 lg:mx-4 mt-2 opacity-60 font-semibold">
               {props.lightButton}
             </button>
           </a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,10 @@ function Navbar(props) {
     // <div className="opacity-100 sticky top-0 z-50">
     <div className={` opacity-100 sticky z-50 top-0  mt-[-70px] lg:mt-[-60px]`}>
       <div className="flex justify-between items-center font-bold px-12 p-4 text-sm">
-        <div className={addBlur + " justify-start  mr-4 lg:mr-0"}>
+        <div className={addBlur + " justify-start  "}>
           <a href="https://tesla.com">
             <img
-              className="h-3"
+              className="h-3 absolute left-3 md:relative"
               src="data:image/svg+xml;base64,PHN2ZyBjbGFzcz0idGRzLWljb24gdGRzLWljb24tbG9nby13b3JkbWFyayB0ZHMtc2l0ZS1sb2dvLWljb24iIHZpZXdCb3g9IjAgMCAzNDIgMzUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTAgLjFhOS43IDkuNyAwIDAgMCA3IDdoMTFsLjUuMXYyNy42aDYuOFY3LjNMMjYgN2gxMWE5LjggOS44IDAgMCAwIDctN0gwem0yMzguNiAwaC02Ljh2MzQuOEgyNjNhOS43IDkuNyAwIDAgMCA2LTYuOGgtMzAuM1Ywem0tNTIuMyA2LjhjMy42LTEgNi42LTMuOCA3LjQtNi45bC0zOC4xLjF2MjAuNmgzMS4xdjcuMmgtMjQuNGExMy42IDEzLjYgMCAwIDAtOC43IDdoMzkuOXYtMjFoLTMxLjJ2LTdoMjR6bTExNi4yIDI4aDYuN3YtMTRoMjQuNnYxNGg2Ljd2LTIxaC0zOHpNODUuMyA3aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxMy44aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxNC4xaDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3pNMzA4LjUgN2gyNmE5LjYgOS42IDAgMCAwIDctN2gtNDBhOS42IDkuNiAwIDAgMCA3IDd6IiBmaWxsPSJjdXJyZW50Q29sb3IiLz48L3N2Zz4="
               alt="Tesla logo"
             />
@@ -54,14 +54,14 @@ function Navbar(props) {
             className="py-1 px-3 hover:rounded hover:bg-black/5">Menu</li>
           </ul>
         </div>
-        <div className="lg:hidden">
+        <div className=" absolute right-3 top-2 md:relative lg:hidden">
           <button onClick={()=>props.toggleMenu()} className="inline-flex items-center rounded-md py-2 px-4 p-2 text-sm font-medium bg-black/5 shadow-sm hover:bg-black/10">
             Menu
           </button>
         </div>
       </div>
       {
-      props.showMenu && <div className=" bg-white absolute right-0 top-0 w-80  z-50  overflow-y-auto menu-div ">
+      props.showMenu && <div className=" bg-white absolute right-0 top-0 w-80  z-50  overflow-y-auto menu-div">
         <div className="flex justify-end pr-8 pt-8">
           <TfiClose 
             onClick={()=>props.toggleMenu()}           
